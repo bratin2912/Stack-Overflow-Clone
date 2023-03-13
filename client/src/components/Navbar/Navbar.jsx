4cf285b516861e95ab48b55025c06613e9c816bc
Bratin Dey <bratindey12345@gmail.com>
2023-03-13 23:09:55 +0530

    the main navbar is successfully created

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import logo from "../../assets/logo.svg";
-import searchlogo from "../../assets/searchlogo.svg"
+import searchlogo from "../../assets/searchlogo.svg";
+import "./Navbar.css"
 const Navbar = () => {
   let User=null
   return (
     <nav>
       <div className='navbar'>
-        <Link to="/" className='nav-item nav-btn'>
+        <Link to="/" className='nav-item nav-logo'>
           <img src={logo} alt="Stack Overflow Logo" />
         </Link>
         <Link to="/" className='nav-item nav-btn'>About</Link>
@@ -15,7 +16,7 @@ const Navbar = () => {
         <Link to="/" className='nav-item nav-btn'>For Teams</Link>
         <form>
           <input type="text" placeholder='Search...'/>
-          <img src={searchlogo} alt="Search Logo" width="18" />
+          <img src={searchlogo} alt="Search Logo" width="18" className='search-icon'/>
         </form>
         {User===null?
         <Link to="/Auth" className='nav-item nav-link'>Login</Link>  :
